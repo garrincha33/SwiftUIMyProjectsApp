d15859519ce408e4c92c6beb655f9052cd67d43e
Richard Price <mrspondooli@gmail.com>
2021-03-17 18:50:30 +0000

    initial commit project setup

---
+++ ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  MyProjectsApp
+//
+//  Created by Richard Price on 17/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
---
+++ MyProjectsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyProjectsAppApp.swift
+//  MyProjectsApp
+//
+//  Created by Richard Price on 17/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct MyProjectsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
