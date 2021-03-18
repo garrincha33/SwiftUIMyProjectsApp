324cd55382e45affb5e1fc953c30c2e291c42088
Richard Price <mrspondooli@gmail.com>
2021-03-18 19:04:40 +0000

    cleanup

--- HomeView.swift
+++ HomeView.swift
@@ -6,12 +6,10 @@
 //
 
 import SwiftUI
-//step 1 create a new homeView
+
 struct HomeView: View {
-    //step 2
     @EnvironmentObject var dataController: DataController
     var body: some View {
-        //step 2 create a quick way to get some sample data created in data controller
         NavigationView {
             VStack {
                 Button("Add Data") {
